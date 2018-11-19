60075ec0ae88a5f9ab0d36df6f000e667d313d2a
Frey Hargarten <harga012@umn.edu>
2018-11-19 11:05:15 -0600

    Initial commit

---
+++ app/chart.js
@@ -0,0 +1,113 @@
+import * as d3 from 'd3';
+import * as c3 from 'c3';
+
+class Chart {
+
+    constructor(target) {
+        this.target = target;
+        this.chartCounts = null;
+    }
+
+    render() {
+        var self = this;
+
+        var padding = {
+            top: 20,
+            right: 40,
+            bottom: 20,
+            left: 60,
+        };
+
+        self.chartCounts = c3.generate({
+            bindto: self.target,
+            padding: padding,
+            data: {
+                xs: {
+                    '16-24': 'x'
+                },
+                columns: [
+                    ['x', 2008,2009,2010,2011,2012,2013,2014,2015,2016,2017,2018,2019],
+                    ['16-24',0.240372671,0.22761708,0.273934031,0.244299674,0.238512949,0.216657881,0.213030433,0.230769231,0.202885063,0.178118393,0.161528977]
+                    // ['16-24', 774,661,681,600,571,411,497,468,436,337,131,null]
+                ],
+                type: 'line',
+                line: {
+                    connectNull: true
+                }
+            },
+            legend: {
+                show: false
+            },
+            line: {
+                connectNull: true
+            },
+            point: {
+                show: true,
+                r: function(d) {
+                    if (d.x == 2018) {
+                        return 6;
+                    } else {
+                        return 2;
+                    }
+                }
+            },
+            color: {
+                pattern: ['#3580A3']
+            },
+            axis: {
+                // rotated: true,
+                y: {
+                    max: 1,
+                    min: 0, 
+                    padding: {
+                        bottom: 0,
+                        top: 0
+                    },
+                    tick: {
+                        count: 4,
+                        values: [0, 0.25, 0.50, 0.75, 1],
+                        format: d3.format('.0%')
+                    }
+                },
+                x: {
+                    padding: {
+                        right: 0,
+                        left: 0
+                    },
+                    tick: {
+                        // rotate: -75,
+                        multiline: false,
+                        // values: [1950, 1958, 1966, 1974, 1982, 1990, 1998, 2006, 2014, 2018, 2022]
+                    },
+                    // height: 40
+                }
+            },
+            grid: {
+                focus: {
+                    show: false
+                },
+                y: {
+                    lines: [{
+                        value: 0.5,
+                        text: '',
+                        position: 'start',
+                        class: 'powerline'
+                    }]
+
+                }
+            },
+            tooltip: {
+                contents: function(d, defaultTitleFormat, defaultValueFormat, color) {
+                    return '<div class="chart-tooltip gray5"><span class="tooltip-label">' + d[0].x + ':</span>' +
+                        '<span class="tooltip-value">' + defaultValueFormat(d[0].value) + '</span></div>'
+                }
+            }
+        });
+
+    }
+}
+
+export {
+    Chart as
+    default
+}
