c44933126f5d335ad2777f1244dd0b429a1d4db6
Frey Hargarten <harga012@umn.edu>
2018-11-28 10:40:22 -0600

    updates

--- app/index.js
+++ app/index.js
@@ -84,125 +84,110 @@ $.urlParam = function(name){
     $(".slide").show();
   }
 
-  mapboxgl.accessToken = 'pk.eyJ1Ijoic2hhZG93ZmxhcmUiLCJhIjoiS3pwY1JTMCJ9.pTSXx_LFgR3XBpCNNxWPKA';
-    var map = new mapboxgl.Map({
-        container: 'map', 
-        style: 'mapbox://styles/shadowflare/ciqzo0bu20004bknkbrhrm6wf',
-        center: [-74.50, 40], 
-        zoom: 9
-    });
-
-      //crime rate map
-    //   mapboxgl.accessToken = 'pk.eyJ1Ijoic2hhZG93ZmxhcmUiLCJhIjoiS3pwY1JTMCJ9.pTSXx_LFgR3XBpCNNxWPKA';
+      mapboxgl.accessToken = 'pk.eyJ1Ijoic2hhZG93ZmxhcmUiLCJhIjoiS3pwY1JTMCJ9.pTSXx_LFgR3XBpCNNxWPKA';
     
-    //   var map = new mapboxgl.Map({
-    //       container: 'map', // container id
-    //       style: 'mapbox://styles/shadowflare/ciqzo0bu20004bknkbrhrm6wf',
-    //       center: [-93.264313, 44.973269], 
-    //       zoom: 10,
-    //       minZoom: 10,
-    //       hash: false
-    //   });
+      var map = new mapboxgl.Map({
+          container: 'map', // container id
+          style: 'mapbox://styles/shadowflare/ciqzo0bu20004bknkbrhrm6wf',
+          center: [-93.264313, 44.973269], 
+          zoom: 10,
+          minZoom: 10,
+          hash: false
+      });
       
-    //   map.addControl(new mapboxgl.NavigationControl());
-    //   map.scrollZoom.disable();
-    //   map.doubleClickZoom.disable();
+      map.addControl(new mapboxgl.NavigationControl());
+      map.scrollZoom.disable();
+      map.doubleClickZoom.disable();
       
-    //   map.on('load', function() {
-      
-        // d3.json("./shapefiles/minneapolis_nb.json", function(error, nb) {
-        //     d3.json("./shapefiles/incidents.geojson", function(error, shootings) {
+      map.on('load', function() {
 
-//        map.addSource('nb', {
-//          type: 'geojson',
-//          data: nb
-//        });
+       map.addSource('nb', {
+         type: 'geojson',
+         data: './shapefiles/minneapolis_nb.json'
+       });
       
-//         map.addLayer({
-//              'id': 'nb-layer',
-//              'interactive': true,
-//              'source': 'nb',
-//              'layout': {},
-//              'type': 'fill',
-//                   'paint': {
-//                  'fill-antialias' : true,
-//                  'fill-opacity': 0.7,
-//                  'fill-color': {
-//                   "property": "maindata_rate",
-//                   "stops": [
-//                     [0, "#dddddd"],
-//                     [1, "#fee0d2"],
-//                     [4, "#fcbba1"],
-//                     [6, "#fc9272"],
-//                     [8, "#fb6a4a"],
-//                     [10, "#ef3b2c"],
-//                     [20, "#cb181d"],
-//                     [30, "#99000d"]
-//                  ]
-//               },
-//                  'fill-outline-color': 'rgba(255, 255, 255, 1)'
-//            }
-//          }, 'place-neighbourhood');
+        map.addLayer({
+             'id': 'nb-layer',
+             'interactive': true,
+             'source': 'nb',
+             'layout': {},
+             'type': 'fill',
+                  'paint': {
+                 'fill-antialias' : true,
+                 'fill-opacity': 0.7,
+                 'fill-color': {
+                  "property": "maindata_rate",
+                  "stops": [
+                    [0, "#dddddd"],
+                    [1, "#fee0d2"],
+                    [4, "#fcbba1"],
+                    [6, "#fc9272"],
+                    [8, "#fb6a4a"],
+                    [10, "#ef3b2c"],
+                    [20, "#cb181d"],
+                    [30, "#99000d"]
+                 ]
+              },
+                 'fill-outline-color': 'rgba(255, 255, 255, 1)'
+           }
+         }, 'place-neighbourhood');
       
-//        map.addSource('shootings', {
-//          type: 'geojson',
-//          data: shootings
-//        });
+       map.addSource('shootings', {
+         type: 'geojson',
+         data: './shapefiles/incidents.json'
+       });
       
-//                map.addLayer({
-//                         "id": "shootings-layer",
-//                         "type": "circle",
-//                         "source": "shootings",
-//                         "paint": {
-//                            "circle-radius": 3,
-//                            "circle-color": 'rgba(150, 150, 150, 0.8)'
-//                         },
-//                         "filter": [
-//                         "==",
-//                         "WeaponCategory",
-//                         "UNARMED"]
-//               });
+               map.addLayer({
+                        "id": "shootings-layer",
+                        "type": "circle",
+                        "source": "shootings",
+                        "paint": {
+                           "circle-radius": 3,
+                           "circle-color": 'rgba(150, 150, 150, 0.8)',
+                        },
+                        "filter": [
+                        "==",
+                        "WeaponCategory",
+                        "UNARMED"]
+              });
       
-//                map.addLayer({
-//                         "id": "shootings-layer2",
-//                         "type": "circle",
-//                         "source": "shootings",
-//                         "paint": {
-//                            "circle-radius": 3,
-//                            "circle-color": 'rgba(66, 134, 244, 0.8)'
-//                         },
-//                         "filter": [
-//                         "!=",
-//                         "WeaponCategory",
-//                         "UNARMED"]
-//               });
+               map.addLayer({
+                        "id": "shootings-layer2",
+                        "type": "circle",
+                        "source": "shootings",
+                        "paint": {
+                           "circle-radius": 3,
+                           "circle-color": 'rgba(66, 134, 244, 0.8)'
+                        },
+                        "filter": [
+                        "!=",
+                        "WeaponCategory",
+                        "UNARMED"]
+              });
       
       
-//       var popup = new mapboxgl.Popup({
-//           closeButton: false,
-//           closeOnClick: false
-//       });
+      var popup = new mapboxgl.Popup({
+          closeButton: false,
+          closeOnClick: false
+      });
       
-//       map.on('mousemove', function(e) {
-//           var features = map.queryRenderedFeatures(e.point, { layers: ['shootings-layer','shootings-layer2'] });
-//           // Change the cursor style as a UI indicator.
-//           map.getCanvas().style.cursor = (features.length) ? 'pointer' : '';
+      map.on('mousemove', function(e) {
+          var features = map.queryRenderedFeatures(e.point, { layers: ['shootings-layer','shootings-layer2'] });
+          // Change the cursor style as a UI indicator.
+          map.getCanvas().style.cursor = (features.length) ? 'pointer' : '';
       
-//           if (!features.length) {
-//               popup.remove();
-//               return;
-//           }
+          if (!features.length) {
+              popup.remove();
+              return;
+          }
       
-//           var feature = features[0];
+          var feature = features[0];
       
-//           // Populate the popup and set its coordinates
-//           // based on the feature found.
-//           popup.setLngLat(e.lngLat)
-//               .setHTML("<div>" + feature.properties.FirstName + " " + feature.properties.LastName + "</div><div>died in " + feature.properties.year + "</div><div>" + feature.properties.WeaponCategory + "</div>")
-//               .addTo(map);
-//       });
-
-//     });
-// });
+          // Populate the popup and set its coordinates
+          // based on the feature found.
+          popup.setLngLat(e.lngLat)
+              .setHTML("<div>" + feature.properties.FirstName + " " + feature.properties.LastName + "</div><div>died in " + feature.properties.year + "</div><div>" + feature.properties.WeaponCategory + "</div>")
+              .addTo(map);
+      });
       
-    //   });
+      });
